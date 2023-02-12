607ba065686fc99f5b3d949883c5f42132b2bc9d
Peter McClelland <peterwmcclelland@gmail.com>
2023-02-12 12:44:07 -0600

    PORT

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ mongoose.connect(
     process.env.DB_URI
     ).then(() => console.log("DataBase Connected!"))
     .then(() => {
-        app.listen(3000);
+        app.listen(process.env.PORT || 3000, (e) => { console.log('Connected!'); });
     }).catch((err)=>console.log(err));
 
 
