0b24bec6ffd19f243f498e38bec3aebebba046a0
Peter McClelland <peterwmcclelland@gmail.com>
2023-04-11 17:35:37 -0500

    de deploy backend

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const router = require("./routes/wine-routes");
+const path = require('path');
+
+require("dotenv").config({ path: path.resolve(__dirname, './.env') });
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/wines", router);// localhost:3000/wines
+
+mongoose.connect(
+    process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+    ).then(() => console.log("DataBase Connected!"))
+    .then(() => {
+        app.listen(PORT, () => {
+            console.warn(`App listening on http://localhost:${PORT}`);
+          });
+
+          
+    }).catch((err)=>console.log(err));
+
+
+
---
+++ backend/routes/wine-routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const Wine = require("../model/wine");
+const winesController = require("../controllers/wines-controller");
+
+router.get("/", winesController.getAllWines);
+router.post("/", winesController.addWine);
+router.get("/:id", winesController.getById);
+router.put("/:id", winesController.updateWine);
+router.delete("/:id", winesController.deleteWine);
+
+
+
+module.exports = router;
