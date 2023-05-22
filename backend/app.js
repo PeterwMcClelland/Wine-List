6529306ae0fade4f4297fecf2bdf5c14453be1d4
Peter McClelland <peterwmcclelland@gmail.com>
2023-05-22 18:43:47 -0500

    heroku deploy

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,15 @@ app.use(express.json());
 app.use(cors());
 app.use("/wines", router);// localhost:3000/wines
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname,'../build','index.html'));
+});
+
 mongoose.connect(
     process.env.MONGODB_URI, {
         useNewUrlParser: true,
